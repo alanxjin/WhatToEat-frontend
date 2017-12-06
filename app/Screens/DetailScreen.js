89eac4ad3b57ef1a6680237a28fc1455e8fd496a
Yuchen He <h869586015@gmail.com>
2017-12-05 19:36:37 -0600

    add google map

--- app/Screens/DetailScreen.js
+++ app/Screens/DetailScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, StatusBar } from 'react-native';
 import { Container, Header, Left, Right, Content, Footer, FooterTab, List, ListItem, Text, Root, View, CardItem, Body, Button, Icon, Title } from 'native-base';
+import MapView from 'react-native-maps';
 import DetailButtonGroup from '../Components/DetailButtonGroup'
 import DetailContent from '../Components/DetailContent'
 
@@ -17,23 +18,28 @@ export default class DetailScreen extends Component {
 
                 <Content style={{ backgroundColor: "#fff" }}>
                     <View style={{ flex: 1, flexDirection: 'column' }}>
-
-                        <View>
-                            <Image style={{ height: 200, flex: 1 }} source={{ uri: 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg' }} />
+                        <View style={{ height: 200 }}>
+                            <Image style={{ flex: 1 }} source={{ uri: 'https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg' }} />
                         </View>
-
-
                         <DetailContent />
 
-
-
+                        <View style={{ height: 200 }}>
+                            <MapView
+                                initialRegion={{
+                                    latitude: 37.78825,
+                                    longitude: -122.4324,
+                                    latitudeDelta: 0.0922,
+                                    longitudeDelta: 0.0421,
+                                }}
+                                style={{ flex: 1 }}
+                            />
+                        </View>
                     </View>
-
                 </Content>
 
                 <Footer style={{ backgroundColor: "#F8F7F7", height: 80 }}>
                     <FooterTab>
-                        <DetailButtonGroup/>
+                        <DetailButtonGroup />
                     </FooterTab>
                 </Footer>
             </Container>
